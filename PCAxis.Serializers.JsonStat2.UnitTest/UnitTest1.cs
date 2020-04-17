8d99c03487d6002391ea78d57234e2ffb8809eae
Johannes <jfi@ssb.no>
2020-04-17 11:00:18 +0200

    Added dummy unit test

---
+++ PCAxis.Serializers.JsonStat2.UnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PCAxis.Serializers.JsonStat2.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+		public void TestMethod1()
+		{
+			//Arrange
+			var a = 0;
+			var b = 0;
+
+			//Act
+
+			//Assert
+			//Check their equality.
+			Assert.AreEqual(a, b);
+
+		}
+	}
+}
